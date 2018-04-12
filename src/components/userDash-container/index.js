ee22a7a6b523f6140959d7b5f9f5b21f05706750
Bessie Arino <brarino@gmail.com>
2018-04-12 16:53:15 -0700

    getting progess done on this file

--- src/components/userDash-container/index.js
+++ src/components/userDash-container/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link, Redirect } from 'react-router-dom';
 
+import { userProfileFetchRequest } from '../../actions/userProfile-actions.js';
 import * as util from './../../lib/util.js';
 
 class UserDashContainer extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
   }
 
@@ -13,15 +14,45 @@ class UserDashContainer extends React.Component {
     return (
       <section className='user-dash-container'>
         <div className='dash-league'>
-          <h2>Leagues</h2>
+
+          <ul className='league-links'>
+            <li><Link to='#'>View All Leagues</Link></li>
+            <li><Link to='#'>Create a New League</Link></li>
+          </ul>
+
+          <h2>My Leagues</h2>
+
+          <div className='my-leagues'>
+            This box will contain links to leagues that the user is part of.
+          </div>
+
         </div>
 
         <div className='dash-group'>
-          <h2>Groups</h2>
+
+          <ul className='group-links'>
+            <li><Link to='#'>View All Groups</Link></li>
+            <li><Link to='#'>Create a New Group</Link></li>
+          </ul>
+
+          <h2>My Groups</h2>
+
+          <div className='my-groups'>
+            This box will container links to groups that the user is part of.
+          </div>
+
         </div>
       </section>
     );
   }
 }
 
-export default UserDashContainer;
+let mapStateToProps = (state) => ({
+  userProfile: state.userProfile,
+});
+
+let mapDispatchToProps = (dispatch) => ({
+  userPhotosFetch: (photos) => dispatch(userProfileFetchRequest()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(UserDashContainer);
