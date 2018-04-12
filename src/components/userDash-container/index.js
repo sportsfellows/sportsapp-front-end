24e39972634d5aee869e6c0a8e0fa67c6efe60d6
Bessie Arino <brarino@gmail.com>
2018-04-12 15:30:32 -0700

    added some containers in the jsx

--- src/components/userDash-container/index.js
+++ src/components/userDash-container/index.js
@@ -12,7 +12,13 @@ class UserDashContainer extends React.Component {
   render() {
     return (
       <section className='user-dash-container'>
+        <div className='dash-league'>
+          <h2>Leagues</h2>
+        </div>
 
+        <div className='dash-group'>
+          <h2>Groups</h2>
+        </div>
       </section>
     );
   }
