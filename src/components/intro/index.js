705988e144cc075f5756698810845188eac301c1
Brian Bixby <brianbixby0@gmail.com>
2018-04-12 17:57:16 -0700

    pushing changes for greg

--- src/components/intro/index.js
+++ src/components/intro/index.js
@@ -3,10 +3,29 @@ import { connect } from 'react-redux';
 import { Link, Redirect } from 'react-router-dom';
 
 import * as util from './../../lib/util.js';
+import Modal from '../modal';
+import UserAuthForm from '../userAuth-form';
+import { signUpRequest, signInRequest } from '../../actions/userAuth-actions.js';
+import { userProfileFetchRequest } from '../../actions/userProfile-actions.js';
 
 class Intro extends React.Component {
   constructor(props){
     super(props);
+
+  }
+
+  handleSignin = user => {
+    let { userProfileFetch, history } = this.props;
+    return this.props.signIn(user)
+      .then(() => userProfileFetch())
+      .then(() => history.push('/gallery'))
+      .catch(util.logError);
+  };
+
+  handleSignup = user => {
+    return this.props.signUp(user);
+      // .then(() => this.props.history.push('/profile'))
+      // .catch(util.logError);
   }
 
   render() {
@@ -14,11 +33,16 @@ class Intro extends React.Component {
     let lebron = require('./../assetts/introLebron.png');
     let curry = require('./../assetts/introCurry.png');
 
+    let launchModal = false;
+    let signup = true;
+    let handleModal = launchModal ? launchModal=false : launchModal=true;
+    let handleComplete = signup ? this.handleSignup : this.handleSignin;
+
     return (
       <div className="intro">
         <section id="introView" className="view introView">
           <div className="container">
-            <h1 className="headline center">BUILD YOUR OWN NBA ALL-STAR TEAM!</h1>
+            <h1 className="headline center">BUILD YOUR OWN BRACKET!</h1>
           </div>
           <div className="banner-image">
             <div className="background">
@@ -33,14 +57,22 @@ class Intro extends React.Component {
               Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
               </p>
               <p className="author">
-                BY BRIAN BIXBY
+                SUB HEADER HERE
               </p>
-              <button id="start-button">
+              <button id="start-button" onClick={handleModal}>
                 START
               </button>
             </div>
           </div>
         </section>
+
+        {util.renderIf(launchModal,
+          <Modal close={() => launchModal=false }>
+          
+            <UserAuthForm signup={signup} onComplete={handleComplete}/>
+
+          </Modal>
+        )}
       </div>
     );
   }
@@ -53,7 +85,9 @@ let mapStateToProps = state => ({
 
 let mapDispatchToProps = dispatch => {
   return {
-
+    signUp: user => dispatch(signUpRequest(user)),
+    signIn: user => dispatch(signInRequest(user)),
+    userProfileFetch: () => dispatch(userProfileFetchRequest()),
   };
 };
 
