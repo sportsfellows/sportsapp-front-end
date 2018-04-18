58bd8f3f4e74cfa5f97718999f552ec857a8852d
kennieU <ken.unterseher@gmail.com>
2018-04-17 18:03:07 -0700

    changed errCB to take err instead of err.status

--- src/components/intro/index.js
+++ src/components/intro/index.js
@@ -33,7 +33,7 @@ class Intro extends React.Component {
       })
       .catch(err => {
         util.logError(err);
-        errCB(err.status);
+        errCB(err);
       });
   };
 
@@ -43,7 +43,7 @@ class Intro extends React.Component {
       .then(() => this.props.userProfileFetch())
       .catch(err => {
         util.logError(err);
-        errCB(err.status);
+        errCB(err);
     });
   };
 
