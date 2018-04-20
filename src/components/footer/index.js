59b744bc638607ae968abbcd804c9f7d20499a54
Bessie Arino <brarino@gmail.com>
2018-04-19 17:43:41 -0700

    styled the comment sections for group/league

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -4,9 +4,19 @@ class Footer extends React.Component {
   render() {
     return (
       <footer className='footer'>
-        <p>
+        <p>Brian Bixby: 
           <a href="https://github.com/brianbixby" rel="noopener noreferrer" target="_blank"><span><i className="fa fa-github"></i></span> </a>
           <a href="https://www.linkedin.com/in/brianbixby1/" rel="noopener noreferrer" target="_blank"><span><i className="fa fa-linkedin"></i></span></a>
+        </p>
+
+        <p>Bessie Arino: 
+          <a href="https://github.com/bishang" rel="noopener noreferrer" target="_blank"><span><i className="fa fa-github"></i></span> </a>
+          <a href="https://www.linkedin.com/in/bessie-a/" rel="noopener noreferrer" target="_blank"><span><i className="fa fa-linkedin"></i></span></a>
+        </p>        
+        
+        <p>Ken Unterseher: 
+          <a href="https://github.com/kennieU" rel="noopener noreferrer" target="_blank"><span><i className="fa fa-github"></i></span> </a>
+          <a href="https://www.linkedin.com/in/ken-unterseher/" rel="noopener noreferrer" target="_blank"><span><i className="fa fa-linkedin"></i></span></a>
         </p>
       </footer>
     );
