4bab39155df02dc55b43b6d8146397852b4092b6
Brian Bixby <brianbixby0@gmail.com>
2018-04-20 04:17:24 -0700

    logged in landing page almost done

--- src/components/helpers/createSection/index.js
+++ src/components/helpers/createSection/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
+import * as util from '../../../lib/util.js';
 
 class CreateSection extends React.Component {
   render() {
     let muscles = require('./../assets/muscles.png');
+    let holla = require('./../assets/holla.png');
     return (
       <div className={`container component-container${this.props.formType}`}>
         <div className='inner-wrapper'>
-          
           <p className='header create-header'>Create Your Own {this.props.formType}! </p>
-          <img className="muscles" src={muscles} />
+
+          {util.renderIf(this.props.formType === 'league',
+            <img className="muscles" src={muscles} />
+          )}
+          
           <p className='text create-text'>Create a {this.props.formType} of your own, and invite your friends!</p>
+          {util.renderIf(this.props.formType === 'group',
+            <img className="holla" src={holla} />
+          )}  
           <button className='button create-button' onClick={this.props.handleCreate}>Create {this.props.formType} </button>
         </div>
       </div>
